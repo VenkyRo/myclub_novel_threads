c1b3aa2083597db01e2416e896b67d4591d8a3f5
venkatesh <sirisettyvenkatesh@gmail.com>
2026-06-16 11:34:44 +0530

    Require login before reading novels

--- server/routes/publicRoutes.js
+++ server/routes/publicRoutes.js
@@ -1,2 +1,22 @@
-import { Router } from 'express';import { listNovels,novelDetails,chapterDetails,approvedComments } from '../controllers/publicController.js';
-const r=Router();r.get('/novels',listNovels);r.get('/novels/:slug',novelDetails);r.get('/chapters/:chapterId',chapterDetails);r.get('/chapters/:chapterId/comments',approvedComments);export default r;
+import { Router } from 'express';
+import {
+  listNovels,
+  novelDetails,
+  chapterDetails,
+  approvedComments,
+} from '../controllers/publicController.js';
+import { protect } from '../middleware/auth.js';
+
+const r = Router();
+
+// Public: anyone can see novel list on home page
+r.get('/novels', listNovels);
+
+// Protected: login required to open novel details and read chapters
+r.get('/novels/:slug', protect, novelDetails);
+r.get('/chapters/:chapterId', protect, chapterDetails);
+
+// Public: approved comments can be visible
+r.get('/chapters/:chapterId/comments', approvedComments);
+
+export default r;
